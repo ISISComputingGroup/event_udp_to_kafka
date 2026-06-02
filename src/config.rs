51eb5a576bbe0f677693330567dad3c52f754c08
Tom Willemsen <tom.willemsen@stfc.ac.uk>
2026-06-02 14:29:08 +0100

    Add metrics

---
+++ src/config.rs
@@ -0,0 +1,78 @@
+use serde::Deserialize;
+use std::collections::HashMap;
+
+#[derive(Debug, Deserialize)]
+pub struct EventUdpToKafkaConfig {
+    /// UDP port to bind to
+    port: u32,
+
+    /// Ip address of the host, address to bind to
+    host_ip: String,
+
+    /// Kafka topic to send the data to
+    dest_kafka_topic: String,
+
+    /// Kafka topic to get data from
+    src_kafka_topic: String,
+
+    /// Script operating mode
+    /// 0 - Consume UDP packets from Kafka SRC topic, process and send back to kafka
+    /// 1 - Gets UDP packets from a local socket binding, processes and kafkas
+    /// This script is mainly designed to function in the Kafka-> Kafka configuration
+    /// With the UDP->Kafka rust buffering via kafka. This gives some failover, and potential throughput options
+    ///
+    mode: Option<u32>,
+
+    /// Filepath to the wiring configuration file (csv)
+    wiring_csv_path: String,
+
+    /// IP and port on which to bind the metrics server.
+    /// Example: `127.0.0.1:8484`
+    metrics_bind_addr: String,
+
+    /// Map of Kafka producer configuration properties. Values should be provided as strings.
+    /// All properties are passed through to `librdkafka`.
+    kafka_producer: HashMap<String, String>,
+
+    /// Map of Kafka consumer configuration properties. Values should be provided as strings.
+    /// All properties are passed through to `librdkafka`.
+    kafka_consumer: HashMap<String, String>,
+}
+
+impl EventUdpToKafkaConfig {
+    pub fn port(&self) -> u32 {
+        self.port
+    }
+
+    pub fn host_ip(&self) -> &str {
+        &self.host_ip
+    }
+
+    pub fn dest_kafka_topic(&self) -> &str {
+        &self.dest_kafka_topic
+    }
+
+    pub fn src_kafka_topic(&self) -> &str {
+        &self.src_kafka_topic
+    }
+
+    pub fn mode(&self) -> u32 {
+        self.mode.unwrap_or(0)
+    }
+
+    pub fn wiring_csv_path(&self) -> &str {
+        &self.wiring_csv_path
+    }
+
+    pub fn metrics_bind_addr(&self) -> &str {
+        &self.metrics_bind_addr
+    }
+
+    pub fn kafka_producer_settings(&self) -> &HashMap<String, String> {
+        &self.kafka_producer
+    }
+
+    pub fn kafka_consumer_settings(&self) -> &HashMap<String, String> {
+        &self.kafka_consumer
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
 use clap::Parser;
+use event_udp_to_kafka::config::EventUdpToKafkaConfig;
+use event_udp_to_kafka::metrics::initialize_metrics;
 use event_udp_to_kafka::{Args, WiringConfigRecord, kafka_udp_process, read_csv};
+use log::info;
 
 #[tokio::main]
 async fn main() {
-    println!("DSG - Rust Data Processor");
-    println!("Processing UDP data into the flatbuffers since 2024");
+    info!("Starting event UDP to Kafka");
+
     env_logger::init();
     let args = Args::parse();
 
-    let filename = args.wiring_csv_path.as_str();
+    let config: EventUdpToKafkaConfig =
+        toml::from_str(&std::fs::read_to_string(args.config).expect("Can't read config file"))
+            .expect("Can't parse config from TOML");
+
+    initialize_metrics(&config).expect("Can't initialize metrics");
+
+    let filename = config.wiring_csv_path();
     let csv_data: Vec<WiringConfigRecord> = read_csv(filename);
 
-    kafka_udp_process(args, csv_data).await;
+    kafka_udp_process(&config, csv_data).await;
 }
